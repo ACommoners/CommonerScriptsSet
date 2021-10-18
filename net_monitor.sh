af7518650930c6a30c1c3c1aeffaeba6976759b8
hello_wen <522390482@qq.com>
2021-10-18 16:09:01 +0800

    file: interface flow monitor

---
+++ net_monitor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash                                                                                                                                                                       
+
+ethn=$1
+
+while true
+do
+  RX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
+  TX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
+  sleep 1
+  RX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
+  TX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
+
+  clear
+#  echo -e "时间:$(date +%k:%M:%S)   |RX|    |TX|"
+
+  RX=$((${RX_next}-${RX_pre}))
+  TX=$((${TX_next}-${TX_pre}))
+
+  if [[ $RX -lt 1024 ]];then
+    RX="${RX}B/s"
+  elif [[ $RX -gt 1048576 ]];then
+    RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
+  else
+    RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
+  fi
+
+  if [[ $TX -lt 1024 ]];then
+    TX="${TX}B/s"
+  elif [[ $TX -gt 1048576 ]];then
+    TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
+  else
+    TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
+  fi
+
+times=$(date +%k:%M:%S)
+echo -e "
+time:        net_name:       RX:              TX:
+${times}     ${ethn}      ${RX}       ${TX}
+"
+done
